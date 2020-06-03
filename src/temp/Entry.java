01c6274ef37c2782c43eab81b3d2d893fa396ba3
indirachakka0702 <indirachakka0702@gmail.com>
2020-06-02 22:12:02 -0600

    first commit

---
+++ src/temp/Entry.java
@@ -0,0 +1,12 @@
+package temp;
+
+public class Entry {
+
+	public static void main(String[] args) 
+	{
+	    cal c = new cal();
+	    System.out.println(c.add(10, 10));
+       System.out.println(c.setName("sri"));
+	}
+
+}
---
+++ src/temp/cal.java
@@ -0,0 +1,18 @@
+package temp;
+
+public class cal 
+{
+    int a=50;
+    String sName= "Nidhi";
+    
+   int add(int a, int b)
+    {
+    	return this.a+b;
+    }
+   public String setName(String sName)
+   {
+	   return sName+this.sName;
+   }
+}
+// using this keyword refer to variable which is there in that class
+// global variable
